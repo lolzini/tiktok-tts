4117515909549411fb71d417339c56e8251ef41b
lolzini <97997498+lolzini@users.noreply.github.com>
2024-04-25 17:06:09 -0600

    fix: add audios to queue instead of new player instance

--- playAudio.mjs
+++ playAudio.mjs
@@ -1,7 +1,7 @@
 import { exec } from "child_process";
 
 export default function playAudio(route) {
-  exec(`vlc ${route} --play-and-exit`, (err, stdout, stderr) => {
+  exec(`vlc ${route} --playlist-enqueue`, (err, stdout, stderr) => {
     if (err) {
       console.error(err);
       return;
