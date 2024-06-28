c8bde25ba502d153e30d99012e55c6e17488f238
lolzini <97997498+lolzini@users.noreply.github.com>
2024-06-27 18:00:19 -0600

    change: play flag to quit vlc after playlist ends

--- playAudio.mjs
+++ playAudio.mjs
@@ -1,7 +1,10 @@
 import { exec } from "child_process";
 
+// --play-and-exit works if VLC is set to Allow only one instance
+// Tools > Preferences > Interface > [x] Allow only one instance
+
 export default function playAudio(route) {
-  exec(`vlc ${route} --playlist-enqueue`, (err, stdout, stderr) => {
+  exec(`vlc ${route} --play-and-exit`, (err, stdout, stderr) => {
     if (err) {
       console.error(err);
       return;
